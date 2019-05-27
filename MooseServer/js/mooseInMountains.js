885463dcd360b8bd0bef557f1baa37dc0420a644
spencer <mathiaspencer@gmail.com>
2019-05-27 16:25:40 -0600

    local host ip

--- MooseServer/js/mooseInMountains.js
+++ MooseServer/js/mooseInMountains.js
@@ -5,7 +5,7 @@
 //events
 
 //network definitions
-const localAddress = '192.168.1.124'
+const localAddress = 'localhost'//'192.168.1.124'
 const localPort = '8080'
 const publicAddress = '184.167.236.159'
 
