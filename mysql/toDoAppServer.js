bb35ecfe7e6a91738039d3cc7bd7fa92f10c00a6
spencer <mathiaspencer@gmail.com>
2020-04-20 16:27:16 -0600

    mysql practice

---
+++ mysql/toDoAppServer.js
@@ -0,0 +1,13 @@
+let mysql = require('mysql');
+let connection = mysql.createConnection({
+	host: 'localhost',
+	user:'root',
+	password:'password',
+	database:'todoapp'
+});
+connection.connect(function(err){
+	if (err) {
+		return console.error('error: '+err.message);
+	}
+	console.log('connected to MySQL server.')
+})
